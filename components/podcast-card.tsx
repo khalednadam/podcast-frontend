bac650fa99a84adca4a1cf2a77b03230c662cc54
Khaled Nadam <khalednadam@gmail.com>
2025-08-17 17:54:58 +0300

    fix podcast card style

--- components/podcast-card.tsx
+++ components/podcast-card.tsx
@@ -1,7 +1,5 @@
 import React, {useMemo} from "react";
 import Image from "next/image";
-import {Button} from "./ui/button";
-import {EllipsisVertical, Play} from "lucide-react";
 import Link from "next/link";
 import {Podcast} from "@/types/podcast";
 import {getGenreColor} from "@/util/getGenreColor";
@@ -10,9 +8,9 @@ const PodcastCard = ({podcast}: { podcast: Podcast }) => {
     return (
         <Link className="flex shrink-0 flex-col items-start justify-start pb-2 w-[200px] md:w-[227px] group" href={podcast.trackViewUrl} target={"_blank"}>
             <Image className="rounded-[3px]" src={podcast.artworkUrl600} alt={podcast.trackName} width={227}
-                   height={268}/>
-            <div className="flex flex-col flex-1 items-start justify-start ">
-                <p className="group-hover:underline truncate !text-start max-w-[250px] text-[14px]">
+                   height={277}/>
+            <div className="flex flex-col flex-1 items-start justify-start">
+                <p className="group-hover:underline truncate !text-start max-w-[190px] text-[14px]">
                     {podcast.trackName}
                 </p>
                 <p className={`text-[12px]`} style={{color: getGenreColor(podcast.genres[0])}}>
